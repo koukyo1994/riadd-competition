697dcdb79088c97954bad2449b38a1bfa9383564
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-02-16 10:52:02 +0900

    Update loss weight

--- exp/exp009_dann.py
+++ exp/exp009_dann.py
@@ -109,7 +109,7 @@ class CFG:
     ######################
     loss_name = "DANNLoss"
     loss_params: dict = {
-        "weights": [1.0, 0.1]
+        "weights": [1.0, 0.01]
     }
 
     ######################
